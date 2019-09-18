25b0f4e350747744a3b48fc3f5f1bfe9d773e079
msardic <msungurardic@gmail.com>
2019-09-18 12:06:17 +0300

    userComment

--- js/index.js
+++ js/index.js
@@ -113,19 +113,12 @@ $(function () {
                 data += '<strong>$15k</strong></div>';
                 data += '<div class="card-text d-flex align-items-center justify-content-between py-2 cardTextStyle">';
                 data += '<p>Collison</p>';
-
                 data += ' <i class="amax ' + iconCollision + '" ></i>';
-
-
-
                 data += '</div><div class="card-text d-flex align-items-center justify-content-between py-2 cardTextStyle">';
                 data += '<p>Comprehensive</p>';
-
                 data += ' <i class="amax ' + iconCompherensive + '" ></i>';
-
                 data += '</div></div><a href="#" class="btn btn-success border-0 btnColor1 text-white my-1">START MY QUOTE</a>';
                 data += '</div></div></div>';
-
 
                 text += data;
 
@@ -162,19 +155,19 @@ $(function () {
                 let questionTitle = element.questionTitle;
                 let answerOfQuestion = element.answerOfQuestion;
                 let svgName = element.svgName;
-                
 
-               
-                let data="";
+
+
+                let data = "";
                 data += '<div class="col-lg-4 col-md-6 col-sm-12 col-xl-4 d-flex flex-column align-items-center px-0 my-5 ">';
                 data += '<div class="questionRectangle">';
-                data += '<div class="questionHeaderSide d-flex"><div class="svgImage mr-2"><img src="./img/'+svgName+'.svg" /></div><div class="questionHeaderText">'+questionTitle+'</div></div>';
-                data += '<div class="questionContentText my-3">'+answerOfQuestion+'</div>';
+                data += '<div class="questionHeaderSide d-flex"><div class="svgImage mr-2"><img src="./img/' + svgName + '.svg" /></div><div class="questionHeaderText">' + questionTitle + '</div></div>';
+                data += '<div class="questionContentText my-3">' + answerOfQuestion + '</div>';
                 data += '<div class="d-flex align-items-baseline"><div class="questionReadMore mr-3">Take the Quiz</div><div class="qRMArrow"><img src="./img/tailright.svg" /></div></div>';
                 data += '<div class="redBaseLine"></div>';
                 data += '</div>';
                 data += '</div>';
-                
+
                 text += data;
 
             });
@@ -197,23 +190,23 @@ $(function () {
                 let informationTitle = element.informationTitle;
                 let informationExplain = element.informationExplain;
                 let svgName = element.svgName;
-                
 
-               
-                let data="";
-                data +='<div class="col-xl-3 col-md-4 col-sm-6 d-flex justify-content-center">';
-                data +='<div class="bgcontent d-flex flex-column align-items-center justify-content-center mb-3 p-3">';
-                data +='<div class="bgcontentImgBackground d-flex justify-content-center align-items-center"><img src="./img/'+svgName+'.svg" alt=""></div>';
-                data +='<div class="bgcontentBigText my-4">'+informationTitle+'</div>';
-                data +='<div class="bgcontentText">'+informationExplain+'</div>';
-                data +='<div class="questionReadMoreContainer d-flex align-items-baseline invisible">';
-                data +='<div class="questionReadMore mr-3 invisible">Read More</div>';
-                data +='<div class="qRMArrow invisible"><img src="./img/tailright.svg" /></div>';
-                data +='</div>';
-                data +='</div>';
-                data +='</div>';
-                data +='</div>';
-                
+
+
+                let data = "";
+                data += '<div class="col-xl-3 col-md-4 col-sm-6 d-flex justify-content-center">';
+                data += '<div class="bgcontent d-flex flex-column align-items-center justify-content-center mb-3 p-3">';
+                data += '<div class="bgcontentImgBackground d-flex justify-content-center align-items-center"><img src="./img/' + svgName + '.svg" alt=""></div>';
+                data += '<div class="bgcontentBigText my-4">' + informationTitle + '</div>';
+                data += '<div class="bgcontentText">' + informationExplain + '</div>';
+                data += '<div class="questionReadMoreContainer d-flex align-items-baseline invisible">';
+                data += '<div class="questionReadMore mr-3 invisible">Read More</div>';
+                data += '<div class="qRMArrow invisible"><img src="./img/tailright.svg" /></div>';
+                data += '</div>';
+                data += '</div>';
+                data += '</div>';
+                data += '</div>';
+
                 text += data;
 
             });
@@ -224,9 +217,62 @@ $(function () {
         });
 });
 
+$(function () {
+    $.getJSON("../json/thirdContent.json",
+
+        function (result) {
+            $(".textContent p:nth-child(1)").append(result.contentTitle);
+
+            $(".textContent p:nth-child(2)").append(result.contentParagraph);
+
+        }
+
+    )
+
+});
+
+$(function () {
+    $.getJSON("../json/userComment.json",
+
+        function (result) {
+
+            let text = "";
+
+            result.forEach(function (element) {
+
+                let customerName = element.customerName;
+                let customerSurname = element.customerSurname;
+                let customerJob = element.customerJob;
+                let customerComment = element.customerComment;
+                let customerPhoto = element.customerPhoto;
+                
+                
+                let data = "";
+
+                data +='<div class="col-xl-4 cl-lg-4 col-md-6 col-sm-12 my-4 d-flex justify-content-center">';
+                data +='<div class="card commentContent">';
+                data +='<div class="card-body d-flex flex-column justify-content-between p-3">';
+                data +='<p class="card-text">'+customerComment+'</p>';
+                data +='<div class="card-text peopleComment d-flex align-items-center"><img class="mr-3" src="'+customerPhoto+'" alt=""></img>';
+                data +='<div class="peopleName">'+customerName+' '+customerSurname+', '+customerJob+'</div>';
+                data +='</div>';
+                data +='</div>';
+                data +='</div>';
+                data +='</div>';
+                
+                
+
+                text += data;
 
 
 
+            });
+            document.getElementById("userComment").innerHTML = text;
+
+
+
+        });
+});
 
 
 
